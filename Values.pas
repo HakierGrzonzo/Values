5a683dd532d95e0df7c6ca2bc83c02a371d0feb9
HakierGrzonzo <grzes869@gmail.com>
2018-05-16 21:18:27 +0200

    ver.0.1 should work

--- Values.pas
+++ Values.pas
@@ -1,20 +1,19 @@
-Unit Values
+Unit Values;
 
 interface
 
 type
 	
-	RealValue = record;
-		vlaue,error:real;
-		exponent:longint;
+	RealValue = record
+		value,error:real;
 	end;
 	
-	IntegerValue = record;
-		value,error,exponent:integer;
+	IntegerValue = record
+		value,error:integer;
 	end;
 	
-	LongIntValue = record;
-		value,error,exponent:longint;
+	LongIntValue = record
+		value,error:longint;
 	end;
 
 function RelativeError(value:RealValue):Real;
@@ -24,6 +23,14 @@ function RelativeError(value:LongintValue):Real;
 function Sum(value1,value2:RealValue):RealValue;
 function Sum(value1,value2:integerValue):integerValue;
 function Sum(value1,value2:LongintValue):LongintValue;
+
+function Dif(value1,value2:RealValue):RealValue;
+function Dif(value1,value2:integerValue):integerValue;
+function Dif(value1,value2:LongintValue):longintValue;
+
+function product(value1,value2:realvalue):realvalue;
+function product(value1,value2:integervalue):integervalue;
+function product(value1,value2:Longintvalue):Longintvalue;
 
 implementation
 
@@ -42,4 +49,60 @@ function RelativeError(value:LongintValue):Real;
 		RelativeError:=value.error/value.value;
 	end;	
 	
+function Sum(value1,value2:RealValue):RealValue;
+	begin
+		Sum.value:=(value1.value+value2.value);
+		Sum.error:=(value1.error+value2.error);
+	end;
+	
+function Sum(value1,value2:integerValue):integerValue;
+	begin
+		Sum.value:=(value1.value+value2.value);
+		Sum.error:=(value1.error+value2.error);
+	end;
+
+function Sum(value1,value2:LongintValue):LongintValue;
+	begin
+		Sum.value:=(value1.value+value2.value);
+		Sum.error:=(value1.error+value2.error);
+	end;
+
+function Dif(value1,value2:RealValue):RealValue;
+	begin
+		dif.value:=(value1.value-value2.value);
+		Dif.error:=(value1.error+value2.error);
+	end;
+
+function Dif(value1,value2:integerValue):integerValue;
+	begin
+		dif.value:=(value1.value-value2.value);
+		Dif.error:=(value1.error+value2.error);
+	end;
+
+function Dif(value1,value2:LongintValue):LongintValue;
+	begin
+		dif.value:=(value1.value-value2.value);
+		Dif.error:=(value1.error+value2.error);
+	end;
+	
+function product(value1,value2:realValue):realValue;
+	begin
+		product.value:=(value1.value*value2.value);
+		product.error:=(RelativeError(value1)+RelativeError(value2))*product.value;
+	end;
+
+function product(value1,value2:LongintValue):LongintValue;
+	begin
+		product.value:=(value1.value*value2.value);
+		product.error:=round(RelativeError(value1)+RelativeError(value2))*product.value;
+	end;
+	
+function product(value1,value2:integerValue):integerValue;
+	begin
+		product.value:=(value1.value*value2.value);
+		product.error:=round(RelativeError(value1)+RelativeError(value2))*product.value;
+	end;
+
+	
+
 end.
